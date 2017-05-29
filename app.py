ac20a74c8b924ab86b014e70ed0c8c667ad491f2
Andrew Hall <andrewhall4@gmail.com>
2017-05-29 17:47:49 -0400

    from venv

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+from sklearn.externals import joblib
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	
+	return render_template('index.html')
+
+@app.route('/getprediction', methods=['POST','GET'])
+def get_prediction():
+		
+	if request.method == 'POST':
+		bmi = request.form.get("BMI")
+		age = request.form.get("Age")
+
+		bmi = float(bmi)
+		age = float(age)
+		x_factors = [bmi, age]
+	
+		model = joblib.load('trained_diabetes_model.pkl')
+		prediction = model.predict(x_factors)
+	return render_template('result.html', prediction = prediction)
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run()
