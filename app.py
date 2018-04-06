8e7022574fb6676fefb104627dc93907223ea528
Andrew Hall <andrew.hall@washpost.com>
2018-04-05 22:22:27 -0400

    first commit

--- app.py
+++ app.py
@@ -10,14 +10,24 @@ def index():
 
 @app.route('/getprediction', methods=['POST'])
 def get_prediction():
-	bmi = request.form.get("BMI")
-	age = request.form.get("Age")
-
-	bmi = float(bmi)
-	age = float(age)
-	x_factors = [bmi, age]
+	amount = request.form.get("amount")
+	term = request.form.get("term")
+	income = request.form.get("income")
+	state = request.form.get("state")
+	rent = request.form.get("rent")
+	delinquencies = request.form.get("delinquencies")
+	fico_low = request.form.get("fico_low")
+	fico_high = request.form.get("fico_high")
+	last_fico_low = request.form.get("last_fico_low")
+	last_fico_high = request.form.get("last_fico_high")
+	credit_lines = request.form.get("credit_lines")
+	revolving_balance = request.form.get("revolving_balance")
+	inquires = request.form.get("inquires")
+	employment = request.form.get("employment")
 	
-	model = joblib.load('trained_diabetes_model.pkl')
+	x_factors = [amount, term, income, state, rent, delinquencies, fico_low, fico_high, last_fico_low, last_fico_high, credit_lines, revolving_balance, inquires, employment]
+	
+	model = joblib.load('trained_interest_rate_model.pkl')
 	prediction = model.predict(x_factors)
 	return render_template('result.html', prediction = prediction)
 
